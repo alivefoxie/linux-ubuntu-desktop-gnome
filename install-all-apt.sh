376a71d78ed8d6bd1c7fae02310f55c6ff238f6b
alivefoxie <alivefoxie@gmail.com>
2017-04-08 06:01:53 +0200

    create

---
+++ install-all-apt.sh
@@ -0,0 +1,3 @@
+sudo apt-get update 
+sudo apt-get -Y upgrade
+sudo apt-get install xbindkeys pv htop mc gpaste gparted nautilus unzip unrar wine git ffmpeg build-essential nodejs curl oracle-java7-installer tasksel p7zip-full p7zip rpm inkscape gnome-control-center gnome-online-accounts mixxx
