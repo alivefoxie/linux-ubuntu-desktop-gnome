157a4284f40d791aa3e7368d9ffd098e2cff4259
alivefoxie <alivefoxie@gmail.com>
2017-04-08 06:43:50 +0200

    Create install_hardcore_mode.sh

---
+++ install_hardcore_mode.sh
@@ -0,0 +1,12 @@
+alias update='sudo apt-get update'    
+alias upgrade='sudo apt-get upgrade'
+alias install='sudo apt-get install'
+alias addapt='sudo add-apt-repository'
+
+addapt ppa:webupd8team/java
+
+update 
+
+upgrade
+
+install xbindkeys pv htop mc gpaste gparted nautilus unzip unrar wine oracle-java8-installer git ffmpeg build-essential nodejs curl tasksel p7zip-full p7zip rpm inkscape gnome-control-center gnome-online-accounts mixxx
