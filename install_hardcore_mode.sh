cdf1230811c7bc9a3254a0751bb2aa8cbc9aced2
alivefoxie <alivefoxie@gmail.com>
2017-04-09 22:30:29 +0200

    Update install_hardcore_mode.sh

--- install_hardcore_mode.sh
+++ install_hardcore_mode.sh
@@ -1,6 +1,6 @@
 alias update='sudo apt-get update'    
 alias upgrade='sudo apt-get upgrade'
-alias install='sudo apt install'
+alias install='sudo apt-get install'
 alias addapt='sudo add-apt-repository'
 alias show='sudo apt show'
 alias showrun='pgrep'
@@ -16,4 +16,11 @@ update
 
 upgrade
 
-install xbindkeys pv htop mc gpaste gparted nautilus unzip unrar wine oracle-java8-installer git ffmpeg build-essential nodejs curl tasksel p7zip-full p7zip rpm inkscape gnome-control-center gnome-online-accounts mixxx
+install python-appindicator python-xdg python-pexpect python-gconf python-gtk2 python-glade2 libxxf86vm1 xbindkeys pv htop mc gpaste gparted nautilus unzip unrar wine oracle-java8-installer git ffmpeg build-essential nodejs curl tasksel p7zip-full p7zip rpm inkscape gnome-control-center gnome-online-accounts mixxx
+
+#flux
+cd /tmp
+git clone "https://github.com/xflux-gui/xflux-gui.git"
+cd xflux-gui
+python download-xflux.py
+sudo python setup.py install
